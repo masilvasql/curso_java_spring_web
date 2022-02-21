97fcd18da037cd8236e00986f709795417bc5ec7
Marcelo Abrão da Silva <masilvasql@gmail.com>
2022-02-20 21:31:33 -0300

    developed layers of Repository, updated Rersources and created services

---
+++ src/main/java/com/marcelo/cursomc/services/CategoriaService.java
@@ -0,0 +1,22 @@
+package com.marcelo.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.marcelo.cursomc.domain.Categoria;
+import com.marcelo.cursomc.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+
+	@Autowired // significa que essa dependência será instanciada pelo Spring
+	private CategoriaRepository repo;
+
+	public Categoria buscar(Integer id) {
+		Optional<Categoria> obj = repo.findById(id);
+		return obj.orElse(null);
+	}
+
+}
