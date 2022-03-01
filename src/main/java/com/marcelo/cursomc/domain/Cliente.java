c71e92db147dce84a855df3f15007ab2079297d6
marcelo.silva <masilvasql@gmail.com>
2022-03-01 12:56:55 -0300

    inicio da criação das classes referente à pedido e seus relacionamentos

--- src/main/java/com/marcelo/cursomc/domain/Cliente.java
+++ src/main/java/com/marcelo/cursomc/domain/Cliente.java
@@ -37,6 +37,8 @@ public class Cliente implements Serializable{
 	@CollectionTable(name="TELEFONE")
 	private Set<String> telefones = new HashSet<>();//não permite repetição de conteúdo
 	
+	private List<Pedido> pedidos = new ArrayList<>();
+	
 	public Cliente() {
 	}
 
@@ -128,6 +130,14 @@ public class Cliente implements Serializable{
 		} else if (!id.equals(other.id))
 			return false;
 		return true;
+	}
+
+	public List<Pedido> getPedidos() {
+		return pedidos;
+	}
+
+	public void setPedidos(List<Pedido> pedidos) {
+		this.pedidos = pedidos;
 	}
 	
 	
